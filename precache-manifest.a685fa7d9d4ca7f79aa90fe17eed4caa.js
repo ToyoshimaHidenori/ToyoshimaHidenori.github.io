528419f79990526ec5a3639a816d1e371daeb085
豊島秀典 <h193345@edu.tut.ac.jp>
2020-05-24 21:32:08 +0900

    Updates

---
+++ precache-manifest.a685fa7d9d4ca7f79aa90fe17eed4caa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85df3ebf23ab587d09cf2492f8e87f03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a88de3af1f1f13054ae1",
+    "url": "/static/css/main.fd0b33d1.chunk.css"
+  },
+  {
+    "revision": "59b89c6e6451ddc6593f",
+    "url": "/static/js/2.9f8a7318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f8a7318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88de3af1f1f13054ae1",
+    "url": "/static/js/main.0f159163.chunk.js"
+  },
+  {
+    "revision": "3c3dd564166aa2dce624",
+    "url": "/static/js/runtime-main.b12b3c7f.js"
+  },
+  {
+    "revision": "fcf81f8d4d513fc377ed108c525ad8bd",
+    "url": "/static/media/face.fcf81f8d.png"
+  }
+]);
