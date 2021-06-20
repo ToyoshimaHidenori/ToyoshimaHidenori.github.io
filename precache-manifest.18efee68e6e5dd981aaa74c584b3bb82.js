c47a95b633d56305f732f878a12189a6f823a3f6
豊島秀典 <h193345@edu.tut.ac.jp>
2021-06-20 18:55:06 +0900

    Modify path /info -> /

---
+++ precache-manifest.18efee68e6e5dd981aaa74c584b3bb82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9637b7959a364e4a0c61b17549adf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1422d728e81f0b3dc1a",
+    "url": "/static/css/main.d94cf6d8.chunk.css"
+  },
+  {
+    "revision": "c2c991f8820945a54de1",
+    "url": "/static/js/2.b2d042f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2d042f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1422d728e81f0b3dc1a",
+    "url": "/static/js/main.601803be.chunk.js"
+  },
+  {
+    "revision": "3c3dd564166aa2dce624",
+    "url": "/static/js/runtime-main.b12b3c7f.js"
+  },
+  {
+    "revision": "d2b51d31fc65fb9266221bb587843d3c",
+    "url": "/static/media/face.d2b51d31.png"
+  }
+]);
